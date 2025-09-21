1c36986140fa3dcd4ca18ad043e1bd9ad8cbb917
David Ford <dford@smart-soft.com>
2025-09-20 19:59:23 -0700

    Get this (the remote part of module federation)

--- src/V7Page.tsx
+++ src/V7Page.tsx
@@ -7,6 +7,11 @@ import axios from 'axios';
 import {QueryClientProvider, useQuery} from '@tanstack/react-query';
 import {QueryClient} from '@tanstack/query-core';
 
+
+// import {createFileRoute} from '@tanstack/react-router';  TODO: Figure this out
+// import z from 'zod';  TODO: Figure this out
+
+
 // TODO: Figure this out
 // export const Route = createFileRoute('/dave')({
 //     validateSearch: z.object({
